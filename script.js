4a1921d3ac2ae1ef84f01d29050b3488e3a6bf4c
Daryl Nauman <darylnauman@gmail.com>
2021-05-19 17:26:09 -0400

    added array of single digit numbers (0 - 9) to be able to include in password

--- script.js
+++ script.js
@@ -23,10 +23,13 @@ var includeNumeric;
 var includeSpecial;
 
 // Arrays of values to create password
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 const specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+","-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[","\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 const alphabetUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const alphabetLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// alphabetLowercase = alphabetUppercase.map(alphabetUppercase => alphabetUppercase.toLowerCase());
+
+console.log(numbers);
+console.log(numbers.length);
 
 function generatePassword() {
   getPasswordLength();
